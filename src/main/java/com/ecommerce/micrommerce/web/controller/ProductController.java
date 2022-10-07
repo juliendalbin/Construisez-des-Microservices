e28457fa475aabebad15208123ca9e67ddbff290
juliendalbin <julien.dalbin@gmail.com>
2022-10-07 10:54:00 +0200

    intialize master

---
+++ src/main/java/com/ecommerce/micrommerce/web/controller/ProductController.java
@@ -0,0 +1,65 @@
+package com.ecommerce.micrommerce.web.controller;
+
+import com.ecommerce.micrommerce.web.dao.ProductDao;
+import com.ecommerce.micrommerce.web.exceptions.ProduitIntrouvableException;
+import com.ecommerce.micrommerce.web.model.Product;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@Api( "API pour les opérations CRUD sur les produits.")
+@RestController
+public class ProductController {
+
+    private final ProductDao productDao;
+
+    public ProductController(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @DeleteMapping (value = "/Produits/{id}")
+    public void supprimerProduit(@PathVariable int id) {
+        productDao.deleteById(id);
+    }
+
+    @PutMapping (value = "/Produits")
+    public void updateProduit(@RequestBody Product product) {
+        productDao.save(product);
+    }
+
+    //Récupérer la liste des produits
+    @GetMapping("/Produits")
+    public List<Product> listeProduits() {
+        return productDao.findAll();
+    }
+
+    @ApiOperation(value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!")
+    @GetMapping(value = "/Produits/{id}")
+    public Product afficherUnProduit(@PathVariable int id) {
+        Product produit = productDao.findById(id);
+        if(produit==null) throw new ProduitIntrouvableException("Le produit avec l'id " + id + " est INTROUVABLE. Écran Bleu si je pouvais.");
+        return produit;
+    }
+
+    @GetMapping(value = "test/produits/{prixLimit}")
+    public List<Product> testeDeRequetes(@PathVariable int prixLimit) {
+        return productDao.findByPrixGreaterThan(prixLimit);
+    }
+
+    @PostMapping(value = "/Produits")
+    public ResponseEntity<Product> ajouterProduit(@RequestBody @Valid Product product) {
+        Product productAdded = productDao.save(product);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productAdded.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
---
+++ src/main/java/com/ecommerce/micrommerce/web/model/Product.java
@@ -0,0 +1,72 @@
+package com.ecommerce.micrommerce.web.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+//@JsonFilter("monFiltreDynamique")
+@Entity
+public class Product {
+    @Id
+    private int id;
+    @Size(min = 3, max = 25)
+    private String nom;
+    @Min(value = 1)
+    private int prix;
+
+    //information que nous ne souhaitons pas exposer
+    private int prixAchat;
+
+    public Product() {
+    }
+
+    public Product(int id, String nom, int prix, int prixAchat) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.prixAchat = prixAchat;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public void setPrixAchat(int prixAchat){
+        this.prixAchat = prixAchat;
+    }
+
+    public int getPrixAchat(){
+        return prixAchat;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                '}';
+    }
+}
